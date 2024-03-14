aeb0730ec8afde7abad24637b5b674f9e26fbd35
Jay Stotz <jay@release.com>
2024-03-14 11:14:31 -0400

    Add neogit

--- mappings.lua
+++ mappings.lua
@@ -4,14 +4,10 @@ local utils = require("user.utils")
 return {
   n = {
     ["<leader>gH"] = {
-      function()
-        toggle_term_cmd(
-          "lazygit log --filter "
-          .. ("%q"):format(utils.git_root_relative_path_current_file())
-        )
-      end,
+      function() toggle_term_cmd("lazygit log --filter " .. ("%q"):format(utils.git_root_relative_path_current_file())) end,
       desc = "File history",
     },
     ["<leader>gl"] = { "<cmd>Git blame<cr>", desc = "View Git blame" },
+    ["<leader>gg"] = false, -- disable default lazygit mapping to remap to neogit
   },
 }
