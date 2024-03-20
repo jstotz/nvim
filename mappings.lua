20050ba9149bc9a3e10f5aab34ec92d17b021d9e
Jay Stotz <jay@release.com>
2024-03-20 09:14:00 -0400

    Use diffview for file history

--- mappings.lua
+++ mappings.lua
@@ -1,10 +1,7 @@
-local toggle_term_cmd = require("astronvim.utils").toggle_term_cmd
-local utils = require("user.utils")
-
 return {
   n = {
     ["<leader>gH"] = {
-      function() toggle_term_cmd("lazygit log --filter " .. ("%q"):format(utils.git_root_relative_path_current_file())) end,
+      "<cmd>:DiffviewFileHistory %<cr>",
       desc = "File history",
     },
     ["<leader>gl"] = { "<cmd>Git blame<cr>", desc = "View Git blame" },
