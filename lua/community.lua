c899f904b71a2fa33ea88da090dea930b323a594
Jay Stotz <jay@release.com>
2024-05-03 16:16:07 -0400

    Add diffview

--- lua/community.lua
+++ lua/community.lua
@@ -9,6 +9,7 @@ return {
   { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.git.octo-nvim" },
+  { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.go" },
