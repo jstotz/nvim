b0746db248780aa4a99cd9528cb427b10f92d5f3
Jay Stotz <jason.stotz@gmail.com>
2024-09-27 09:27:47 -0400

    Update plugins

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -58,4 +58,16 @@ return {
       { "<leader>gB", "<CMD>Gitsigns blame<CR>", { desc = "Blame file" } },
     },
   },
+  {
+    "gbprod/substitute.nvim",
+    opts = {},
+    config = function(_plugin, opts)
+      require("substitute").setup(opts)
+
+      vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
+      vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
+      vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
+      vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
+    end,
+  },
 }
