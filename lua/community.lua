043df3ae02160e9b3d0dcb3e17ea95c6be85945c
Jay Stotz <jason.stotz@gmail.com>
2024-11-04 09:48:19 -0500

    Marks

--- lua/community.lua
+++ lua/community.lua
@@ -11,6 +11,7 @@ return {
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.git.octo-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.motion.marks-nvim" },
   { import = "astrocommunity.motion.mini-ai" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.bash" },
