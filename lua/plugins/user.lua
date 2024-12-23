3a81fb2762d6868d31209758a2f92065f70e73bc
Jay Stotz <jason.stotz@gmail.com>
2024-12-23 12:08:19 -0500

    Remove unused luasnip config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -31,18 +31,6 @@ return {
       require("alpha").setup(opts.config)
     end,
   },
-
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      -- include the default astronvim config that calls the setup call
-      require "astronvim.plugins.configs.luasnip"(plugin, opts)
-      -- load snippets paths
-      require("luasnip.loaders.from_vscode").lazy_load {
-        paths = { vim.fn.stdpath "config" .. "/snippets" },
-      }
-    end,
-  },
   {
     "stevearc/oil.nvim",
     opts = {},
